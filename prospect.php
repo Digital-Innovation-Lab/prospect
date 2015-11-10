44d7cc0d1d7605e16ff249d0694df653b40ae270
Newton, Michael <msnewton@email.unc.edu>
2015-11-10 10:27:05 -0500

    Bumped version

--- prospect.php
+++ prospect.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Prospect
  * Plugin URI:        https://prospect.web.unc.edu/
  * Description:       Digital Humanities platform for visualizing curated collections
- * Version:           0.8.5
+ * Version:           0.8.7
  * Author:            Michael Newton, Breon Williams, Digital Innovation Lab, UNC-CH
  * Author URI:        
  * Text Domain:       dil-prospect
