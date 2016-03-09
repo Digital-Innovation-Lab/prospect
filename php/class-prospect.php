78707e0343f4f010e26c06e507ca1b1241a77e99
Michael Newton <msnewton@unc.edu>
2016-03-09 09:31:35 -0500

    Bumped version to 1.0.4

--- php/class-prospect.php
+++ php/class-prospect.php
@@ -281,7 +281,7 @@ class Prospect {
 	public function __construct()
 	{
 		$this->plugin_slug = 'prsp-slug';
-		$this->version = '1.0.3';
+		$this->version = '1.0.4';
 
 		$this->load_dependencies();
 
