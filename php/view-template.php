33e2f048d118f38cf796c61bfdd0172047835542
Michael Newton <msnewton@unc.edu>
2016-08-29 17:20:02 -0400

    Improved styles for Template Record display, handle Date output

--- php/view-template.php
+++ php/view-template.php
@@ -19,6 +19,9 @@
 ?>
 
 <div id="primary" class="content-area">
+	<?php
+	echo('<h1 class="prospect">'.$the_template->def->l.'</h1><hr/>');
+	?>
     <main id="main" class="site-main<?php 	// Add class to main content?
 		switch($display_style) {
 		case 'l':
@@ -33,8 +36,6 @@
 
 		<?php
 		if ($the_template != null) {
-			echo('<h1 class="prospect">'.$the_template->def->l.'</h1><hr/>');
-
 				// Get dependent Templates needed for Joins
 			$d_templates = $the_template->get_dependent_templates(true);
 				// Get associative array for all Attribute definitions
@@ -75,9 +76,9 @@
 						if ($display_image != 'disable' && isset($the_rec->att_data[$display_image])) {
 							echo('<img class="prospect-thumb" src="'.$the_rec->att_data[$display_image].'">');
 						}
-						echo('<p class="prospect-card-text"><span style="font-weight: bold"><a href="'.get_permalink($the_rec->post_id).'">'.$the_rec->label.'</a></span>');
+						echo('<p class="prospect-card-text"><span class="title"><a href="'.get_permalink($the_rec->post_id).'">'.$the_rec->label.'</a></span>');
 						if ($display_content != 'disable' && isset($the_rec->att_data[$display_content])) {
-							echo('<br/>'.prospect_att_val($assoc_atts, $display_content, $the_rec->att_data));
+							echo('<br/><span class="content">'.prospect_att_val($assoc_atts, $display_content, $the_rec->att_data).'</span>');
 						}
 						echo('</p></div>');
 						break;
@@ -90,7 +91,7 @@
 						echo('</a>');
 						echo('<figcaption class="prospect"><div>'.$the_rec->label);
 						if ($display_content != 'disable' && isset($the_rec->att_data[$display_content])) {
-							echo('<br/><span style="font-size: .75em">'.prospect_att_val($assoc_atts, $display_content, $the_rec->att_data).'</span>');
+							echo('<br/><span class="content">'.prospect_att_val($assoc_atts, $display_content, $the_rec->att_data).'</span>');
 						}
 						echo('</div></figcaption>');
 						echo('</figure>');
@@ -111,8 +112,54 @@
 				return implode(", ", $att_val);
 			case 'T':
 			case 'N':
-			case 'D':
 				return $att_val;
+			case 'D':
+				if ($att_val == '?') {
+					return __('(uncertain)', 'prospect');
+				}
+				$date_string = '';
+				$date_part = $att_val['min'];
+				if (!isset($att_val['max'])) { // just a single date
+					if ($date_part['f']) {
+						$date_string = __('about', 'prospect').' ';
+					}
+					$date_string .= $date_part['y'];
+					if (isset($date_part['m'])) {
+						$date_string .= '-'.$date_part['m'];
+						if (isset($date_part['d'])) {
+							$date_string .= '-'.$date_part['d'];
+						}
+					}
+				} else {	// from and to
+					if ($date_part['f']) {
+						$date_string = __('about', 'prospect').' ';
+					}
+					$date_string .=  $date_part['y'];
+					if (isset($date_part['m'])) {
+						$date_string .= '-'.$date_part['m'];
+						if (isset($date_part['d'])) {
+							$date_string .= '-'.$date_part['d'];
+						}
+					}
+
+					$date_part = $att_val['max'];
+					$date_string .= ' '.__('to', 'prospect').' ';
+					if ($date_part == 'open') {
+						$date_string .= __('now', 'prospect').' ';
+					} else {
+						if ($date_part['f']) {
+							$date_string .= __('about', 'prospect').' ';
+						}
+						$date_string .= $date_part['y'];
+						if (isset($date_part['m'])) {
+							$date_string .= '-'.$date_part['m'];
+							if (isset($date_part['d'])) {
+								$date_string .= '-'.$date_part['d'];
+							}
+						}
+					} // define to date
+				} // from and to
+				return $date_string;
 			} // switch Attribute type
 		} // prospect_att_val()
 
