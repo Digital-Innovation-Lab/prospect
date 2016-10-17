4db23bd12b85c6a80f8d6f577221713bbbccac19
Kevin Jacoby <kvnjcby@gmail.com>
2016-10-17 17:15:01 -0400

    Filter results

--- prospect.php
+++ prospect.php
@@ -805,7 +805,7 @@ function tmplt_shortcode($atts)
 			'content_attr' => null,		// attribute id of additional content to be displayed (optional)	
 			'filter_attr_id' => null,			// filter attribute id (optional)
 			'filter_attr_val' => null,			// filter attribute value (optional)
-			'filter_attr_comp' => '=='		// filter attribute comparison (==, !=)
+			'filter_attr_comp' => '='		// filter attribute comparison (=, !=)
 	), $atts);
 
 		// Return null if no template id is provided
@@ -1057,6 +1057,24 @@ function tmplt_shortcode($atts)
 	$d_templates = $the_template->get_dependent_templates(true);
 		// Get associative array for all Attribute definitions
 	$assoc_atts = ProspectAttribute::get_assoc_defs();
+
+		// Set up meta_query for use in $args
+	$meta_query = array(
+					array(
+						'key' => 'tmplt-id',
+						'value' => $a['template'],
+						'compare' => '='
+					)
+	);
+		// Provide filter if defined
+	if (array_key_exists($a['filter_attr_id'], $assoc_atts) && $a['filter_attr_val'] != null && ($a['filter_attr_comp'] == '=' || $a['filter_attr_comp'] == '!=')) {
+		$meta_query[] = array(
+			'key' => $a['filter_attr_id'],
+			'value' => $a['filter_attr_val'],
+			'compare' => $a['filter_attr_comp']
+		);
+	}
+
 		// Get Records -- Need to order by Record ID, etc
 	$args = array('post_type' => 'prsp-record',
 					'post_status' => 'publish',
@@ -1064,10 +1082,7 @@ function tmplt_shortcode($atts)
 					'orderby' => 'meta_value',
 					'order' => 'ASC',
 					'posts_per_page' => -1,
-					'meta_query' =>
-						array('key' => 'tmplt-id',
-							'value' => $a['template'],
-							'compare' => '=')
+					'meta_query' => $meta_query
 				);
 	$query = new WP_Query($args);
 	if ($query->have_posts()) {
@@ -1081,7 +1096,7 @@ function tmplt_shortcode($atts)
 				if ($a['image_attr'] != null && isset($the_rec->att_data[$a['image_attr']])) {
 					$html .= '<img class="prospect-thumb" src="'.$the_rec->att_data[$a['image_attr']].'">';
 				}
-				if ($a['content_attr'] != null && isset($the_rec->att_data[$a['image_attr']])) {
+				if ($a['content_attr'] != null && isset($the_rec->att_data[$a['content_attr']])) {
 					$html .= '<p class="prospect-list-content">'.get_att_val($assoc_atts, $a['content_attr'], $the_rec->att_data).'</p>';
 				}
 				$html .= '</div>';
@@ -1112,8 +1127,6 @@ function tmplt_shortcode($atts)
 				$html .= '</figure>';
 				break;
 			}
-
-
 		} // foreach
 	} // if have_posts
 
