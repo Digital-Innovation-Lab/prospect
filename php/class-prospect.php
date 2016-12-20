7fb82966c3a0bf0345b568f533a1bdf33a15c6f1
Michael Newton <msnewton@unc.edu>
2016-12-20 12:56:49 -0500

    Load minified versions of files

--- php/class-prospect.php
+++ php/class-prospect.php
@@ -129,8 +129,8 @@ class Prospect {
 
 			wp_enqueue_script('d3', plugins_url('lib/d3.min.js', dirname(__FILE__)));
 
-			wp_enqueue_script('prsp-view-core', plugins_url('js/view-core.js', dirname(__FILE__)), array('underscore'));
-			wp_enqueue_script('prsp-view-exhibit', plugins_url('js/view-exhibit.js', dirname(__FILE__)), array('prsp-view-core'));
+			wp_enqueue_script('prsp-view-core', plugins_url('js/view-core.min.js', dirname(__FILE__)), array('underscore'));
+			wp_enqueue_script('prsp-view-exhibit', plugins_url('js/view-exhibit.min.js', dirname(__FILE__)), array('prsp-view-core'));
 
 				// Get Exhibit definition
 			$the_xhbt = new ProspectExhibit(true, get_the_ID(), true);
@@ -155,7 +155,6 @@ class Prospect {
 				case 'm':
 					$use_aggregate = true;
 					break;
-				// case 'L':
 				case 'Q':
 					$use_maps = true;
 				// case 'q':
@@ -172,7 +171,7 @@ class Prospect {
 				wp_enqueue_script('prsp-view-aggregate', plugins_url('js/view-aggregate.min.js', dirname(__FILE__)), array('prsp-view-core'));
 			}
 			if ($use_qr) {
-				wp_enqueue_script('prsp-view-qr', plugins_url('js/view-qr.js', dirname(__FILE__)), array('prsp-view-core'));
+				wp_enqueue_script('prsp-view-qr', plugins_url('js/view-qr.min.js', dirname(__FILE__)), array('prsp-view-core'));
 			}
 
 				// Check for optional settings
