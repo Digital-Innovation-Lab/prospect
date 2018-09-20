ff1d8aabca0e0e09da2f8802b0e74bcd640f6300
charles7 <charleshiggins2112@gmail.com>
2018-09-20 16:58:32 -0400

    empty

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import npm from "rollup-plugin-node-resolve";
-
-export default {
-  entry: "d3-package.js",
-  format: "iife",
-  moduleName: "d3",
-  plugins: [npm({jsnext: true})],
-  dest: "lib/d3.js"
-};
